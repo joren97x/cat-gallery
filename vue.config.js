4fa3783c58b12ec34d0d3f13956c922d3814e51e
joren97x <babamyloves2002@gmail.com>
2023-07-28 09:20:46 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
 
-  publicPath: 'cat-gallery',
+  publicPath: '/cat-gallery/',
 
   pluginOptions: {
     vuetify: {
